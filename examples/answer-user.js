490e1b5ced41c1ce58f8f24ffac26c8f3dfbae9a
CodeDotJS <rushforlinux@gmail.com>
2016-08-13 23:43:35 +0530

    Revert "get answer answered by a user on a particular question"
    This reverts commit 4570ab7a6e70eee3b50a85cd326b85907a443d5d.

--- examples/answer-user.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const quora = require('../index.js');
-
-quora.answer.user('What-was-the-first-program-Adam-DAngelo-wrote/answer/Adam-DAngelo').then(answer => {
-	console.log(answer);
-});
